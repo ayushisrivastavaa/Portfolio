61a9ae1b4c2e4bcf4945074ee72293a98112b961
ayushisrivastavaa <ayushisrivastava1028@gmail.com>
2024-07-17 21:19:00 +0530

    profile

---
+++ src/component/herosection.js
@@ -0,0 +1,24 @@
+export default function HeroSection(){
+    return(
+        <section id ="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section--content">
+                    <p className="section--title">Hey, I'm Ayushi</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title-color">Full Stack</span>{" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section--description">
+                        
+                        <br />
+                    </p>
+                </div>
+                <button className="btn-primary btn">Get In Touch</button>
+            </div>
+            <div className="hero--section--img">
+              
+            </div>
+        </section>
+    )
+}
